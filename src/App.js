71db60cc94ddb8c1b2acd4eb72b2b23e1dc6ddf7
Gabbie Bade <gabbiebade@gmail.com>
2023-01-04 12:56:59 -0800

    Added Helmet to update site title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import Screen from './components/Screen';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
@@ -12,6 +12,9 @@ function App() {
 
   return (
     <div>
+      <Helmet>
+        <title>Calculator App</title>
+      </Helmet>
       <Header title="Calculator App"/>
       {/* Add <Screen /> here if needed, was originally duplicating bounding box */}
       <Main />
